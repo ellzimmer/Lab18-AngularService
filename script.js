daa280ef18c46f16a0821d740caff1ea00139457
Ellen Zimmer <ellenzimmer711@gmail.com>
2015-11-23 20:02:49 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var app = angular.module('serviceModule', ['ngRoute']);
+
+app.config(function($routeProvider){
+  
+    $routeProvider.when('/input',{
+      	controller: 'controlInput',
+   		templateUrl:'input.html',
+})
+
+    .when('/display',{
+      	controller: 'controlDisplay',
+ 	    templateUrl:'display.html',
+    })
+
+    .otherwise ({redirectTo:'/input'});
+});
